922f7c2cd46d48f77bd264e8377616f3841744e2
David Thiede <davidrthiede@gmail.com>
2019-11-08 13:35:47 -0800

    Executes job.run() via par_iter().

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
+use chrono;
+use rand;
 use rayon::prelude::*;
-use std::default::Default;
-#[derive(Debug, Default)]
-struct Job {
-    filename: String,
+use std::{thread, time};
+
+#[derive(Debug, Copy, Clone)]
+struct Job<'a> {
+    filename: &'a str,
+    created: chrono::DateTime<chrono::Utc>,
 }
-impl Job {
-    fn new() -> Self {
-        Default::default()
-    }
-    fn add(s: &str) -> Self {
-        Job {
-            filename: s.to_string(),
+impl<'a> Job<'a> {
+    fn new(name: &'a str) -> Self {
+        Self {
+            filename: name,
+            created: chrono::Utc::now(),
         }
     }
+    fn run(self) {
+        let r = rand::random::<u8>();
+        let delay = time::Duration::from_millis(r as u64);
+        thread::sleep(delay);
+    }
 }
+
 fn main() {
     println!("Hello, world!");
-    let mut jobs: Vec<Job> = vec!();
-    jobs.insert(0, Job::add("one"));
-    jobs.push(Job::add("two"));
-    jobs.push(Job::add("three"));
-    jobs.insert(0, Job::add("four"));
+    let mut jobs: Vec<Job> = vec![];
+    jobs.insert(0, Job::new("one"));
+    jobs.push(Job::new("two"));
+    jobs.push(Job::new("three"));
+    jobs.insert(0, Job::new("four"));
 
-    let jobiter = jobs.par_iter_mut();
+    let jobiter = jobs.par_iter().map(|&x|x.run());
     dbg!(&jobiter);
 }
